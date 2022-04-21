7af2ff0b651835848374ac9d8ea8a0f12c2938bc
ShrutiDiwan1403 <shrutisindhi1403@gmail.com>
2022-04-21 23:05:02 +0100

    Initial Commit

---
+++ Auth/_init_.py
@@ -0,0 +1,24 @@
+import pyrebase
+
+# Configuration
+config = {
+  "apiKey": "AIzaSyDvqkK0qG_Mhdh2AgMwZcWNUfIcsvpHZCU",
+  "authDomain": "assignment2-21973.firebaseapp.com",
+  "databaseURL": "https://assignment2-21973-default-rtdb.firebaseio.com",
+  "projectId": "assignment2-21973",
+  "storageBucket": "assignment2-21973.appspot.com",
+  "messagingSenderId": "625821177645",
+  "appId": "1:625821177645:web:ecf64eb22a9ea7a619e5a4"
+}
+
+# Initialize firebase
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
+
+request_user = {
+  "is_logged_in": False,
+  "name": "",
+  "email": "",
+  "uid": ""
+}
